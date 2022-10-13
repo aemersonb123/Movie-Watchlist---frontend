acb4b9154aa9e6ce7a85c1822f2be663a1187472
Aiden-Bostwick <aidenbostwick5@gmail.com>
2022-10-13 03:47:37 +0200

    Create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar loggedIn={false} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState } from 'react';
+
+function Navbar({ loggedIn }) {
+  const [page, setPage] = useState('movies');
+
+  const getStylesNavItem = (itemPage) => {
+    return 'nav-item ' + (itemPage === page ? 'nav-item-active' : 'null');
+  };
+
+  return (
+    <div className='navbar navbar-expand-lg'>
+      <div className='container-fluid'>
+        <a className='navbar-brand'>Movie Watchlist</a>
+        <ul className='navbar-nav'>
+          <li className={getStylesNavItem('movies')}>Movies</li>
+          {loggedIn && (
+            <>
+              <li className={getStylesNavItem('watchlist')}>Watchlist</li>
+              <li className='nav-item'>Logout</li>
+            </>
+          )}
+
+          {!loggedIn && (
+            <>
+              <li className={getStylesNavItem('login')}>Login</li>
+              <li className={getStylesNavItem('register')}>Register</li>
+            </>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
